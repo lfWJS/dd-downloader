1c58f85bfee33195a7c50d4f6648528778ce1882
Lou@WJS <109100957+lfWJS@users.noreply.github.com>
2024-02-27 14:33:04 -0500

    fixed unix timestamp issue

--- pkg/datadog/dd.go
+++ pkg/datadog/dd.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
@@ -13,14 +14,16 @@ import (
 
 func GetDataDogLogs(filter model.DataDogFilter, cur *string, limit int32) datadogV2.LogsListResponse {
 	log.Printf("query=>%s, from %d, to %d\n", filter.Query, filter.From, filter.To)
+	unixTo := time.Unix(int64(filter.To), 0)
+	unixFrom := time.Unix(int64(filter.From), 0)
 	body := datadogV2.LogsListRequest{
 		Filter: &datadogV2.LogsQueryFilter{
 			Query: datadog.PtrString(filter.Query),
 			Indexes: []string{
 				"main",
 			},
-			From: datadog.PtrString("2024-01-28T00:00:00+00:00"),
-			To:   datadog.PtrString("2024-02-27T00:00:00+00:00"),
+			From: datadog.PtrString(unixFrom.GoString()),
+			To:   datadog.PtrString(unixTo.GoString()),
 		},
 		Sort: datadogV2.LOGSSORT_TIMESTAMP_ASCENDING.Ptr(),
 		Page: &datadogV2.LogsListRequestPage{
